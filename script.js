d885e1064722182f45e5e07b09d1829d0bbcfce2
RyanWood2334 <ryanwooduwcoding.2334@gmail.com>
2023-03-31 14:04:19 -0700

    added html css and js files

---
+++ script.js
@@ -0,0 +1,42 @@
+// make a button that starts the game
+// make container for button
+//
+//create variables to target page elements
+//timer
+//win/loss tracker
+//random words
+//guesses
+//start button
+//reset button
+//
+//
+//function for:
+//      render each iteration to DOM
+
+//need timer
+//make containers for timer
+//function for timer
+//maybe put timer and win/loss in same container
+//          prevent default on timer/win loss
+//          if statement for when timer runs out
+//              include messages for winning and/or losing ---- include timer stopping
+//  
+//write function for random words
+//make container for missing letters
+//write function to compare letters
+//      this will probably be an if statement"if letter is correct replace blank with letter"
+//          use the "matches" method with keyboard eventlistener?
+//
+//write function for storing to local storage
+//write function for accessing local storage on page load
+//
+//
+//
+//eventlistener: keyboardevent
+//eventlistener for button start
+//
+//
+
+
+var timer = document.querySelector()
+var winLossTracker = document.querySelector()
